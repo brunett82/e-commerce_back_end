e37dfaf0b99fcd1c9a69a2ecda5d5b3323759322
Rob Brunett <rb3@Robs-Air.lan>
2021-06-26 13:05:06 -0400

    category get it route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -24,10 +24,29 @@ router.get('/', (req, res) => {
     res.status(500).json(err)
   });
 });
-
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
+// find one category by its `id` value
   // be sure to include its associated Products
+router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  })
+  .then(dbCatData => {
+    if(!dbCatData) {
+      res.status(404).json({message: 'Category Not Found'});
+      return;
+    }
+    res.json(dbCatData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
 });
 
 router.post('/', (req, res) => {
