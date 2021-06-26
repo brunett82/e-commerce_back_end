70fbba6a64fc269b47a65db6383c2957ebcf6a0d
Rob Brunett <rb3@Robs-Air.lan>
2021-06-26 13:40:04 -0400

    tag get id route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -17,10 +17,23 @@ router.get('/', (req, res) => {
     res.status(500).json(err);
   });
 });
-
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
+ // find a single tag by its `id`
   // be sure to include its associated Product data
+router.get('/:id', (req, res) => {
+ Tag.findOne({
+   where: {
+     id: req.params.id
+   },
+   include: {
+     model: Product,
+     attributes: ['product_name', 'price', 'stock', 'category_id']
+   }
+ })
+ .then(dbTagData => res.json(dbTagData))
+ .catch(err => {
+   console.log(err);
+   res.status(500).json(err);
+ });
 });
 
 router.post('/', (req, res) => {
